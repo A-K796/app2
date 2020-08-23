0b639a678430bb9ac30bcdc128164cdb8d701676
A-K796 <69107060+A-K796@users.noreply.github.com>
2020-08-23 21:38:51 +0900

    finish5-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a3a4c8b729749d6072059d3f238e63f987bfe181bb556109a98cd1fd720a252971afcc0f8b7d8045fecdf40f09bdefe2b1177644fcfcd8322936f93b395daa8b'
